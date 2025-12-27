9e751d998bb36f7c1c1a7f6f3d5a5f5e627671f4
VANI20345 <aboody.sh.2003@gmail.com>
2025-12-27 19:53:57 +0300

    Add files via upload

---
+++ src/constants/defaults.ts
@@ -0,0 +1,12 @@
+// Default values for the application
+export const DEFAULT_CITY = {
+  id: '85dbebf1-27af-433c-b4ae-2fe7a3b35459',
+  name: 'Jeddah',
+  name_ar: 'جدة'
+};
+
+// Referral reward amount in SAR
+export const REFERRAL_REWARD_AMOUNT = 10;
+
+// Shield member badge for first 1000 beta users
+export const SHIELD_MEMBER_LIMIT = 1000;
