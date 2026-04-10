c6df8af017dd9b0fe3b77274b68fc2b6a78e7aa0
Abdulqader <aboody.sh.2003@gmail.com>
2026-04-10 23:28:30 +0300

    a

--- supabase/migrations/20251202172043_aed0030c-c797-4c6d-a74e-ecc2e581d368.sql
+++
@@ -1,5 +0,0 @@
--- Add INSERT policy for notifications table to allow users to send notifications
-CREATE POLICY "Users can create notifications for others" 
-ON public.notifications 
-FOR INSERT 
-WITH CHECK (true);
