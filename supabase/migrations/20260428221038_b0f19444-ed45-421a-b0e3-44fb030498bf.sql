836696b6b87ff33b243f344b462ec683aec50873
VANI20345 <aboody.sh.2003@gmail.com>
2026-05-02 10:26:45 +0300

    a

--- supabase/migrations/20260428221038_b0f19444-ed45-421a-b0e3-44fb030498bf.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.system_settings (key, value)
-VALUES ('feature_toggles', '{"groups": true, "services": true, "trainings": true, "discounts": true}'::jsonb)
-ON CONFLICT (key) DO NOTHING;
